dcb0e57232e2cece000f895bae39180b6c7bb40b
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2026-01-23 00:57:35 +0200

    Add final optimal solution to 121.best-time-to-buy-and-sell-stock

--- 121.best-time-to-buy-and-sell-stock/src/main/java/org/example/Main.java
+++ 121.best-time-to-buy-and-sell-stock/src/main/java/org/example/Main.java
@@ -1,44 +1,56 @@
 package org.example;
 
+
+import static java.lang.Math.max;
+
 public class Main {
     public static int maxProfit(int[] prices) {
-        int l = 0, r = prices.length - 1, maxP, maxPL, maxPR;
+        int profit = 0, buy_price = prices[0];
 
-        for(int i = 1; i < prices.length; ++i){
-            maxP = -9999;
-            maxPL = -1;
-            maxPR = -1;
-            if(prices[r] - prices[l] < prices[prices.length - 1 - i] - prices[i] && i <= prices.length - 1 - i){
-                maxPL = i;
-                maxPR = prices.length - 1 - i;
-                maxP = prices[prices.length - 1 - i] - prices[i];
-            }
-            if(prices[r] - prices[l] < prices[prices.length - 1 - i] - prices[l] && l <= prices.length - 1 - i)
-                if(maxP < prices[prices.length - 1 - i] - prices[l]){
-                    maxPL = l;
-                    maxPR = prices.length - 1 - i;
-                    maxP = prices[prices.length - 1 - i] - prices[l];
-                }
+        for(int i = 0; i < prices.length; ++i){
+            if(buy_price > prices[i])
+                buy_price = prices[i];
 
-            if(prices[r] - prices[l] < prices[r] - prices[i] && i <= r)
-                if(maxP < prices[r] - prices[i]){
-                    maxPL = i;
-                    maxPR = r;
-                    maxP = prices[r] - prices[i];
-                }
-
-            if(maxP > prices[r] - prices[l]){
-                l = maxPL;
-                r = maxPR;
-            }
+            profit = max(profit, prices[i] - buy_price);
         }
 
-        if(prices[r] - prices[l] < 0)
-            return 0;
-        return (prices[r] - prices[l]);
+        return profit;
     }
 
     public static void main(String[] args) {
         System.out.println(maxProfit(new int[]{2, 7, 1, 4}));
     }
 }
+/*
+121. Best Time to Buy and Sell Stock
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+ */
