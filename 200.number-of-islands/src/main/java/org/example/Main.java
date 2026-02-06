c4b8626c941653881745b198cd6e52082daf9e30
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2026-02-06 17:05:28 +0200

    Final solution of 200.number-of-islands Runtime                                         4                                         ms                                         Beats                                         48.91%

--- 200.number-of-islands/src/main/java/org/example/Main.java
+++ 200.number-of-islands/src/main/java/org/example/Main.java
@@ -5,49 +5,49 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Main {
-    static class Pair {
-        Integer first;
-        Integer second;
+    public static void removeIslands(int y, int x, char[][] grid){
+        Queue<int[]> queue = new ArrayDeque<>();
 
-        Pair(Integer first, Integer second) {
-            this.first = first;
-            this.second = second;
-        }
-    }
+        queue.add(new int[]{y, x});
 
-    public static void removeIcland(int y, int x, char[][] grid){
-        Queue<Pair> queue = new ArrayDeque<>();
-
-        queue.add(new Pair(y, x));
-
-        Pair current;
+        int[] current;
 
         while (!queue.isEmpty()){
             current = queue.poll();
-            grid[current.first][current.second] = '0';
+            grid[current[0]][current[1]] = '0';
 
-            if(current.second - 1 >= 0 && grid[current.first][current.second-1] == '1')
-                queue.add(new Pair(current.first, current.second-1));
-            if(current.second + 1 < grid[0].length && grid[current.first][current.second+1] == '1')
-                queue.add(new Pair(current.first, current.second+1));
-            if(current.first - 1 >= 0 && grid[current.first-1][current.second] == '1')
-                queue.add(new Pair(current.first-1, current.second));
-            if(current.first + 1 < grid.length && grid[current.first+1][current.second] == '1')
-                queue.add(new Pair(current.first+1, current.second));
+            if(current[1] - 1 >= 0 && grid[current[0]][current[1]-1] == '1'){
+                queue.add(new int[]{current[0], current[1]-1});
+                grid[current[0]][current[1]-1] = '0';
+            }
+
+            if(current[1] + 1 < grid[0].length && grid[current[0]][current[1]+1] == '1'){
+                queue.add(new int[]{current[0], current[1]+1});
+                grid[current[0]][current[1]+1] = '0';
+            }
+            if(current[0] - 1 >= 0 && grid[current[0]-1][current[1]] == '1'){
+                queue.add(new int[]{current[0]-1, current[1]});
+                grid[current[0]-1][current[1]] = '0';
+            }
+
+            if(current[0] + 1 < grid.length && grid[current[0]+1][current[1]] == '1'){
+                queue.add(new int[]{current[0]+1, current[1]});
+                grid[current[0]+1][current[1]] = '0';
+            }
         }
     }
 
     public static int numIslands(char[][] grid) {
-        int step = 0, res = 0;
+        int res = 0;
 
-        for(var i : grid){
-            for(int j = 0; j < i.length; ++j){
-                if(grid[step][j] == '0')
+        for(int i = 0; i < grid.length; ++i){
+            for(int j = 0; j < grid[i].length; ++j){
+                if(grid[i][j] == '0')
                     continue;
-                removeIcland(step, j, grid);
+
+                removeIslands(i, j, grid);
                 ++res;
             }
-            ++step;
         }
 
         return res;
@@ -55,10 +55,67 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(numIslands(new char[][]{
-                {'1','1','0','0','0'},
-                {'1','1','0','0','0'},
-                {'0','0','1','0','0'},
-                {'0','0','0','1','1'}
+                {'1','1','1','1','1','0','1','1','1','1','1','1','1','1','1','0','1','0','1','1'},
+                {'0','1','1','1','1','1','1','1','1','1','1','1','1','0','1','1','1','1','1','0'},
+                {'1','0','1','1','1','0','0','1','1','0','1','1','1','1','1','1','1','1','1','1'},
+                {'1','1','1','1','0','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+                {'1','0','0','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+                {'1','0','1','1','1','1','1','1','0','1','1','1','0','1','1','1','0','1','1','1'},
+                {'0','1','1','1','1','1','1','1','1','1','1','1','0','1','1','0','1','1','1','1'},
+                {'1','1','1','1','1','1','1','1','1','1','1','1','0','1','1','1','1','0','1','1'},
+                {'1','1','1','1','1','1','1','1','1','1','0','1','1','1','1','1','1','1','1','1'},
+                {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+                {'0','1','1','1','1','1','1','1','0','1','1','1','1','1','1','1','1','1','1','1'},
+                {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+                {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+                {'1','1','1','1','1','0','1','1','1','1','1','1','1','0','1','1','1','1','1','1'},
+                {'1','0','1','1','1','1','1','0','1','1','1','0','1','1','1','1','0','1','1','1'},
+                {'1','1','1','1','1','1','1','1','1','1','1','1','0','1','1','1','1','1','1','0'},
+                {'1','1','1','1','1','1','1','1','1','1','1','1','1','0','1','1','1','1','0','0'},
+                {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+                {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+                {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'}
         }));
     }
 }
+
+/*
+200. Number of Islands
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+
+
+Example 1:
+
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+Example 2:
+
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] is '0' or '1'.
+ */
