0a446a7ebcfbc3b23ad22c0ee9ee4b936d24ab9a
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2026-02-08 22:26:08 +0200

    Add second try 207.course-schedule Runtime                                    184                                    ms                                    Beats                                    5.26%

--- 207.course-schedule/src/main/java/org/example/Main.java
+++ 207.course-schedule/src/main/java/org/example/Main.java
@@ -1,24 +1,54 @@
 package org.example;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
-    public static boolean checkPrereq(int[][] prerequisites, Set<Integer> nums ,int number){
-        for(var i : prerequisites){
-            if(i[0] == number){
-                if(!nums.contains(i[1])){
-                    return false;
-                }
-            }
+    public static boolean checkPrereq(Map<Integer, List<Integer>> prereqMap, Set<Integer> nums, int number){
+        List<Integer> val = new ArrayList<>();
+
+        val = prereqMap.get(number);
+
+        if(val == null)
+            return true;
+
+        for(var i : val){
+            if(!nums.contains(i))
+                return false;
         }
 
         return true;
     }
 
+    public static Map<Integer, List<Integer>> fillMap(Map<Integer, List<Integer>> prereqMap, int[][] prerequisites){
+        List<Integer> val = new ArrayList<>();
+
+        for(var i : prerequisites){
+            if(prereqMap.containsKey(i[0])){
+                val = prereqMap.get(i[0]);
+
+                val.add(i[1]);
+
+                prereqMap.put(i[0], val);
+            }
+            else{
+                val = new ArrayList<>(){};
+                val.add(i[1]);
+                prereqMap.put(i[0], val);
+            }
+        }
+
+        return prereqMap;
+    }
+
     public static boolean canFinish(int numCourses, int[][] prerequisites) {
         Set<Integer> nums = new HashSet<>();
         int setSize = -1;
+
+        Map<Integer, List<Integer>> prereqMap = new HashMap<>();
+
+        prereqMap = fillMap(prereqMap, prerequisites);
+
+
 
         while (true){
             if(nums.size() == setSize)
@@ -28,7 +58,7 @@ public class Main {
 
             for(int i = 0; i < numCourses; ++i){
                 if(!nums.contains(i)){
-                    if(checkPrereq(prerequisites, nums, i)){
+                    if(checkPrereq(prereqMap, nums, i)){
                         nums.add(i);
                     }
                 }
